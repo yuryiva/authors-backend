106082dc0ceeff8d19f11d9b1ab4f34c64742b30
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 16:37:57 +0300

    added the-authors.netlify.com

--- payment/constants/frontend.js
+++ payment/constants/frontend.js
@@ -1,13 +1,11 @@
 // const FRONTEND_DEV_URLS = ["http://localhost:3000"];
 // const FRONTEND_PROD_URLS = ["http://localhost:3000", "http://localhost:3000"];
 
-// const FRONTEND_DEV_URLS = ["https://the-authors.netlify.app"];
-// const FRONTEND_PROD_URLS = ["https://the-authors.netlify.app", "https://the-authors.netlify.app"];
-
-const FRONTEND_DEV_URLS = ["https://festive-rosalind-4c72b4.netlify.app"];
-const FRONTEND_PROD_URLS = ["https://festive-rosalind-4c72b4.netlify.app", "https://festive-rosalind-4c72b4.netlify.app"];
+const FRONTEND_DEV_URLS = ["https://the-authors.netlify.app"];
+const FRONTEND_PROD_URLS = ["https://the-authors.netlify.app", "https://the-authors.netlify.app"];
 
 module.exports =
+console.log('process.env.NODE_ENV' = process.env.NODE_ENV)
   process.env.NODE_ENV === "production"
     ? FRONTEND_PROD_URLS
     : FRONTEND_DEV_URLS;

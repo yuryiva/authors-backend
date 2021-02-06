40ff11afdf923bf0f6b60126051a9602d69f37e2
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 17:04:30 +0300

    5

--- payment/constants/frontend.js
+++ payment/constants/frontend.js
@@ -4,10 +4,11 @@
 const FRONTEND_DEV_URLS = ["https://the-authors.netlify.app"];
 const FRONTEND_PROD_URLS = ["https://the-authors.netlify.app", "https://the-authors.netlify.app"];
 
-console.log("process.env.NODE_ENV =", process.env.NODE_ENV)
+console.log("process.env.NODE_ENV which comes to payment/constants/frontend.js = ", process.env.NODE_ENV)
 ////****process.env.NODE_ENV values comes here from heroku config settings********
+////**** it is there set as production by default, but can be manually changed to another one*****
 module.exports =
-  process.env.NODE_ENV === "production333"
+  process.env.NODE_ENV === "production"
     ? FRONTEND_PROD_URLS
     : FRONTEND_DEV_URLS;
 

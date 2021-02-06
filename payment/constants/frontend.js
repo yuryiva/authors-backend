53fd5597fee2c706cf6c1f14d35dfe84f33ce5e2
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 17:21:35 +0300

    added choice in frontend.js

--- payment/constants/frontend.js
+++ payment/constants/frontend.js
@@ -1,12 +1,13 @@
-// const FRONTEND_DEV_URLS = ["http://localhost:3000"];
 // const FRONTEND_PROD_URLS = ["http://localhost:3000", "http://localhost:3000"];
 
-const FRONTEND_DEV_URLS = ["https://the-authors.netlify.app"];
+const FRONTEND_DEV_URLS = ["http://localhost:3000"];
 const FRONTEND_PROD_URLS = ["https://the-authors.netlify.app", "https://the-authors.netlify.app"];
 
 console.log("process.env.NODE_ENV which comes to payment/constants/frontend.js = ", process.env.NODE_ENV)
 ////****process.env.NODE_ENV values comes here from heroku config settings********
-////**** it is there set as production by default, but can be manually changed to another one*****
+////**** it is there set as production by default, but inside heroku can be manually changed to another one*****
+////***by changing production to development on line 12 you can switch to localhost if needed****
+
 module.exports =
   process.env.NODE_ENV === "production"
     ? FRONTEND_PROD_URLS

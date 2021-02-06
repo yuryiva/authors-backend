a44b2e37070882598087dc68bea7fab18d549000
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 16:55:10 +0300

    4

--- payment/constants/frontend.js
+++ payment/constants/frontend.js
@@ -4,9 +4,10 @@
 const FRONTEND_DEV_URLS = ["https://the-authors.netlify.app"];
 const FRONTEND_PROD_URLS = ["https://the-authors.netlify.app", "https://the-authors.netlify.app"];
 
-console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+console.log("process.env.NODE_ENV =", process.env.NODE_ENV)
+////****process.env.NODE_ENV values comes here from heroku config settings********
 module.exports =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "production333"
     ? FRONTEND_PROD_URLS
     : FRONTEND_DEV_URLS;
 

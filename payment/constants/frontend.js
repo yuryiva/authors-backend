6b96e923c9d48b1fa16554909a3c22de52c12f7d
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-07 14:41:17 +0300

    fixed the-authors.netlify.app

--- payment/constants/frontend.js
+++ payment/constants/frontend.js
@@ -1,7 +1,7 @@
 // const FRONTEND_PROD_URLS = ["http://localhost:3000", "http://localhost:3000"];
 
 const FRONTEND_DEV_URLS = ["http://localhost:3000"];
-const FRONTEND_PROD_URLS = ["https://the-authors2.netlify.app", "https://the-authors2.netlify.app"];
+const FRONTEND_PROD_URLS = ["https://the-authors.netlify.app", "https://the-authors.netlify.app"];
 
 console.log("process.env.NODE_ENV which comes to payment/constants/frontend.js = ", process.env.NODE_ENV)
 ////****process.env.NODE_ENV values comes here from heroku config settings********

ed14f681cd020224764cc95a70d5d8fb2656c6ae
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-07 13:09:39 +0300

    locally works

--- payment/constants/frontend.js
+++ payment/constants/frontend.js
@@ -1,6 +1,6 @@
 // const FRONTEND_PROD_URLS = ["http://localhost:3000", "http://localhost:3000"];
 
-const FRONTEND_DEV_URLS = ["http://localhost:3001"];
+const FRONTEND_DEV_URLS = ["http://localhost:3000"];
 const FRONTEND_PROD_URLS = ["https://the-authors2.netlify.app", "https://the-authors2.netlify.app"];
 
 console.log("process.env.NODE_ENV which comes to payment/constants/frontend.js = ", process.env.NODE_ENV)
